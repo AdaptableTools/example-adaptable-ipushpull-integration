9f6a022110e8f7dd2f3814ed46b19aa84423dd20
Bogdan Petru Pintcan <bogdanpintican@gmail.com>
2022-02-14 10:30:03 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# iPushPull <> AdapTable Integration Example
+
+This demo shows how you can use ipushpull inside AdapTable.
+
+The code is using TypeScript for convenience - but it could easily be just JavaScript.
+
+## Installation
+
+NOTE: In order to be able to run `npm install`, you need to follow the [AdapTable installation instructions](https://github.com/AdaptableTools/adaptable/blob/master/packages/adaptable/README.md) - so you need to be logged into our private NPM registry.
+
+> If you do not have an Adpatable Login please contact support@adaptabletools.com
+
+Run `npm install` (or `yarn`), depending on what tool you're using.
+
+## Running for development
+
+Before running, if you want to prefill your ipushpull username & password in the demo, you can create a `.env` file, with the following contents:
+
+```sh
+IPUSHPULL_USERNAME="" # optional, can leave as empty string - if passed, will prefill your username in the IPushPull login dialog
+IPUSHPULL_PASSWORD="" # optional, can leave as empty string - if passed, will prefill your password in the IPushPull login dialog
+```
+
+You can run the project by executing the following command
+
+```sh
+$ npm run dev
+```
+
+**NOTE**: The first time you run this, it takes longer as parcel generates some cache, etc - subsequent runs will be a lot faster
+
+Now navigate to **[localhost:1234](http://localhost:1234)** in order the see AdapTable in action.
+
+Any change you make in your sources will trigger a browser reload, since parceljs comes with built-in hot-reloading in order to keep you productive.
+
+## Licences
+A licence for AdapTable provides access to all product features as well as quarterly updates and enhancements through the lifetime of the licence, comprehensive support, and access to all 3rd party libraries.
+
+Licences can be purchased individually, for a team (minimum 30 end-users), for an organisation or for integration into software for onward sale.
+
+We can make a trial licence available for a short period of time to allow you to try out AdapTable for yourself.
+
+Please contact [`sales@adaptabletools.com`](mailto:sales@adaptabletools.com) for more information.
+ 
+## Demo
+
+To see AdapTable in action visit our [Demo Site](https://demo.adaptabletools.com).  Here you can see a large number of AdapTable demos each showing a different feature, function or option in AdapTable (using dummy data sets).
+
+## Help
+
+Developers can learn how to access AdapTable programmatically at [AdapTable Documentation](https://docs.adaptabletools.com).
+
+## More Information
+
+General information about Adaptable Tools is available at our [Website](http://www.adaptabletools.com) 
+ 
+## Support
+
+For all support enquiries please email [`support@adaptabletools.com`](mailto:support@adaptabletools.com) or [raise a Support Ticket](https://adaptabletools.zendesk.com/hc/en-us/requests/new).
---
+++ index.ts
@@ -0,0 +1,115 @@
+import Adaptable from "@adaptabletools/adaptable/agGrid";
+
+import "@adaptabletools/adaptable/index.css";
+import "@adaptabletools/adaptable/themes/dark.css";
+import ipp from "@adaptabletools/adaptable-plugin-ipushpull";
+import { IPushPullApi } from "@adaptabletools/adaptable/src/Api/IPushPullApi";
+import finance from "@adaptabletools/adaptable-plugin-finance";
+import charts from "@adaptabletools/adaptable-plugin-charts";
+
+import { AllEnterpriseModules } from "@ag-grid-enterprise/all-modules";
+import "@ag-grid-community/all-modules/dist/styles/ag-grid.css";
+import "@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css";
+import "@ag-grid-community/all-modules/dist/styles/ag-theme-balham-dark.css";
+
+import {
+  AdaptableOptions,
+  PredefinedConfig,
+  AdaptableApi,
+} from "@adaptabletools/adaptable/types";
+
+const columnDefs = [
+  { field: "OrderId", type: "abColDefNumber" },
+  {
+    field: "CompanyName",
+
+    type: "abColDefString",
+  },
+  {
+    field: "ContactName",
+    type: "abColDefString",
+  },
+  {
+    field: "Employee",
+    type: "abColDefString",
+  },
+  {
+    field: "InvoicedCost",
+    editable: true,
+    type: "abColDefNumber",
+  },
+];
+
+const rowData: any[] = [
+  {
+    OrderId: 1,
+    CompanyName: "IBM",
+    ContactName: "Joe Bloggs",
+    Employee: "Mary Shields",
+    InvoicedCost: 32.53,
+  },
+];
+
+let demoConfig: PredefinedConfig = {
+  Dashboard: {
+    Tabs: [
+      {
+        Name: "Dashboard Toolbars",
+        Toolbars: ["Layout", "IPushPull", "Export"],
+      },
+    ],
+  },
+};
+
+const adaptableOptions: AdaptableOptions = {
+  primaryKey: "OrderId",
+  userName: "Demo User",
+  adaptableId: "IPushPull Integration Demo",
+
+  plugins: [
+    finance(),
+    charts(),
+    ipp({
+      throttleTime: 5000,
+      includeSystemReports: true,
+      autoLogin: true,
+      ippConfig: {
+        api_url: "https://www.ipushpull.com/api/1.0",
+        ws_url: "https://www.ipushpull.com",
+        web_url: "https://www.ipushpull.com",
+        docs_url: "https://docs.ipushpull.com",
+        storage_prefix: "ipp_local",
+        transport: "polling",
+        hsts: false, // strict cors policy
+      },
+    }),
+  ],
+  // vendorGrid: { ...gridOptions, modules: AllEnterpriseModules },
+  vendorGrid: {
+    columnDefs,
+    rowData,
+    enableRangeSelection: true,
+    modules: AllEnterpriseModules,
+  },
+  predefinedConfig: demoConfig,
+};
+
+Adaptable.init(adaptableOptions).then((adaptableApi) => {
+  const ipushpullApi: IPushPullApi = adaptableApi.pluginsApi.getPluginApi(
+    "ipushpull"
+  );
+
+  // we simulate server loading when ready
+  adaptableApi.eventApi.on("AdaptableReady", () => {
+    // we load the json orders
+    import("./orders.json")
+      .then((data) => data.default)
+      .then((data) => {
+        // add an extra timeout
+        setTimeout(() => {
+          // and then set the correct row data
+          adaptableApi.gridApi.getVendorGrid().api.setRowData(data);
+        }, 500);
+      });
+  });
+});
