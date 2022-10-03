22e0c7d5b68489e072a586a59f1a5dab0c94b49e
Andrei Floricel <andrei.floricel@gmail.com>
2022-10-03 17:56:11 +0300

    update to latest versions

--- index.ts
+++ index.ts
@@ -1,50 +1,60 @@
-import Adaptable from "@adaptabletools/adaptable/agGrid";
+import '@ag-grid-community/core/dist/styles/ag-grid.css';
+import '@ag-grid-community/core/dist/styles/ag-theme-balham.css';
+import '@ag-grid-community/core/dist/styles/ag-theme-balham-dark.css';
 
-import "@adaptabletools/adaptable/index.css";
-import "@adaptabletools/adaptable/themes/dark.css";
-import ipp from "@adaptabletools/adaptable-plugin-ipushpull";
-import { IPushPullApi } from "@adaptabletools/adaptable/src/Api/IPushPullApi";
-import finance from "@adaptabletools/adaptable-plugin-finance";
+import '@adaptabletools/adaptable/index.css';
+import '@adaptabletools/adaptable/themes/dark.css';
 
-import { AllEnterpriseModules, ColDef } from "@ag-grid-enterprise/all-modules";
-import "@ag-grid-community/all-modules/dist/styles/ag-grid.css";
-import "@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css";
-import "@ag-grid-community/all-modules/dist/styles/ag-theme-balham-dark.css";
+import { Module, ColDef } from '@ag-grid-community/core';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { SparklinesModule } from '@ag-grid-enterprise/sparklines';
+import { GridChartsModule } from '@ag-grid-enterprise/charts';
+import { ClipboardModule } from '@ag-grid-enterprise/clipboard';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { StatusBarModule } from '@ag-grid-enterprise/status-bar';
+import { RichSelectModule } from '@ag-grid-enterprise/rich-select';
+import { SideBarModule } from '@ag-grid-enterprise/side-bar';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
 
-import {
-  AdaptableOptions,
-  PredefinedConfig,
-  AdaptableApi,
-} from "@adaptabletools/adaptable/types";
+import Adaptable from '@adaptabletools/adaptable/agGrid';
+import ipp from '@adaptabletools/adaptable-plugin-ipushpull';
+import { IPushPullApi } from '@adaptabletools/adaptable/src/Api/IPushPullApi';
+import finance from '@adaptabletools/adaptable-plugin-finance';
+
+import { AdaptableOptions, PredefinedConfig } from '@adaptabletools/adaptable/types';
 
 const columnDefs: ColDef[] = [
-  { field: "OrderId", type: "abColDefNumber" },
+  { field: 'OrderId', type: 'abColDefNumber' },
   {
-    field: "CompanyName",
+    field: 'CompanyName',
 
-    type: "abColDefString",
+    type: 'abColDefString',
   },
   {
-    field: "ContactName",
-    type: "abColDefString",
+    field: 'ContactName',
+    type: 'abColDefString',
   },
   {
-    field: "Employee",
-    type: "abColDefString",
+    field: 'Employee',
+    type: 'abColDefString',
   },
   {
-    field: "InvoicedCost",
+    field: 'InvoicedCost',
     editable: true,
-    type: "abColDefNumber",
+    type: 'abColDefNumber',
   },
 ];
 
 const rowData: any[] = [
   {
     OrderId: 1,
-    CompanyName: "IBM",
-    ContactName: "Joe Bloggs",
-    Employee: "Mary Shields",
+    CompanyName: 'IBM',
+    ContactName: 'Joe Bloggs',
+    Employee: 'Mary Shields',
     InvoicedCost: 32.53,
   },
 ];
@@ -53,8 +63,8 @@ let demoConfig: PredefinedConfig = {
   Dashboard: {
     Tabs: [
       {
-        Name: "Dashboard Toolbars",
-        Toolbars: ["Layout", "IPushPull", "Export"],
+        Name: 'Dashboard Toolbars',
+        Toolbars: ['Layout', 'IPushPull', 'Export'],
       },
     ],
   },
@@ -62,17 +72,17 @@ let demoConfig: PredefinedConfig = {
     Revision: Date.now(),
     Reports: [
       {
-        Name: "Invoice greater than 500",
-        ReportColumnScope: "AllColumns",
-        ReportRowScope: "ExpressionRows",
+        Name: 'Invoice greater than 500',
+        ReportColumnScope: 'AllColumns',
+        ReportRowScope: 'ExpressionRows',
         Query: {
-          BooleanExpression: "[InvoicedCost] > 500",
+          BooleanExpression: '[InvoicedCost] > 500',
         },
       },
       {
-        Name: "Wilman Kala Contracts",
-        ReportColumnScope: "AllColumns",
-        ReportRowScope: "ExpressionRows",
+        Name: 'Wilman Kala Contracts',
+        ReportColumnScope: 'AllColumns',
+        ReportRowScope: 'ExpressionRows',
         Query: {
           BooleanExpression: '[CompanyName] = "Wilman Kala"',
         },
@@ -82,9 +92,9 @@ let demoConfig: PredefinedConfig = {
 };
 
 const adaptableOptions: AdaptableOptions = {
-  primaryKey: "OrderId",
-  userName: "Demo User",
-  adaptableId: "IPushPull Integration Demo",
+  primaryKey: 'OrderId',
+  userName: 'Demo User',
+  adaptableId: 'IPushPull Integration Demo',
   licenseKey: process.env.REACT_APP_ADAPTABLE_LICENSE_KEY,
 
   plugins: [
@@ -94,12 +104,12 @@ const adaptableOptions: AdaptableOptions = {
       includeSystemReports: true,
       autoLogin: true,
       ippConfig: {
-        api_url: "https://www.ipushpull.com/api/1.0",
-        ws_url: "https://www.ipushpull.com",
-        web_url: "https://www.ipushpull.com",
-        docs_url: "https://docs.ipushpull.com",
-        storage_prefix: "ipp_local",
-        transport: "polling",
+        api_url: 'https://www.ipushpull.com/api/1.0',
+        ws_url: 'https://www.ipushpull.com',
+        web_url: 'https://www.ipushpull.com',
+        docs_url: 'https://docs.ipushpull.com',
+        storage_prefix: 'ipp_local',
+        transport: 'polling',
         hsts: false, // strict cors policy
       },
     }),
@@ -116,19 +126,31 @@ const adaptableOptions: AdaptableOptions = {
     rowData,
     enableRangeSelection: true,
   },
-  modules: AllEnterpriseModules,
   predefinedConfig: demoConfig,
 };
 
-console.log({ demoConfig });
+export const agGridModules: Module[] = [
+  ClientSideRowModelModule,
+  SideBarModule,
+  ColumnsToolPanelModule,
+  FiltersToolPanelModule,
+  StatusBarModule,
+  MenuModule,
+  RangeSelectionModule,
+  RichSelectModule,
+  ExcelExportModule,
+  GridChartsModule,
+  SparklinesModule,
+  RowGroupingModule,
+  ClipboardModule,
+];
 
-Adaptable.init(adaptableOptions).then((adaptableApi) => {
-  const ipushpullApi: IPushPullApi =
-    adaptableApi.pluginsApi.getipushpullPluginApi();
+Adaptable.init(adaptableOptions, { agGridModules }).then((adaptableApi) => {
+  const ipushpullApi: IPushPullApi = adaptableApi.pluginsApi.getipushpullPluginApi();
   // we simulate server loading when ready
-  adaptableApi.eventApi.on("AdaptableReady", () => {
+  adaptableApi.eventApi.on('AdaptableReady', () => {
     // we load the json orders
-    import("./orders.json")
+    import('./orders.json')
       .then((data) => data.default)
       .then((data) => {
         // add an extra timeout
